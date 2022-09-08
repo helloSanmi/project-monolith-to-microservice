dece3626cd5e3b9aee5798567a4812b5e2227afc
helloSanmi <idowuoluwasanmi@yahoo.com>
2022-09-08 16:03:50 +0100

    new commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad36051cb1ba249a29148116ce021668-1151055507.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
